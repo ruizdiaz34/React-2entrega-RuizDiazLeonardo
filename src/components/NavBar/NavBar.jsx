402b10c2a63b7f6caa8b3c21ad6ed27a7a4bfc69
leonardoruizdiaz <ruizdiazleonardo2023@gmail.com>
2023-11-22 23:27:15 -0300

    inicio commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+
+
+function NavBar (){
+    return (
+        <nav>
+            <ul>
+                <li><a href="#"></a>inicio</li>
+                <li><a href="#"></a>promos</li>
+                <li><a href="#"></a>productos</li>
+                <li><a href="#"></a>nosotros</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
